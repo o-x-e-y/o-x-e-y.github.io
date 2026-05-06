427f127a3fdfb3a2f2b16cc2fe97d267eb0839b0
oxey <lucoerlemans37@gmail.com>
2026-05-06 13:00:02 +0200

    fix home button

--- src/components/HomeButton.tsx
+++ src/components/HomeButton.tsx
@@ -3,8 +3,8 @@ import { A } from '@solidjs/router';
 export default function HomeButton() {
   return (
     <div class="absolute top-0 left-0">
-      <div class="relative w-32 h-16 border-r border-b border-[#666] rounded-br-lg bg-[#393939] flex items-center justify-center">
-        <span class="font-bold text-[#ddd] text-2xl">Home</span>
+      <div class="relative w-[10vw] h-[5vw] min-w-12 min-h-6 border-r border-b border-[#666] rounded-br-lg sm:rounded-br-2xl bg-[#393939] flex items-center justify-center contain-inline-size">
+        <span class="font-bold text-[#ddd] text-[2.67cqw]">Home</span>
         <A href="/" class="absolute inset-0" />
       </div>
     </div>
